7215eb436e79f8a631f13670d94d79b7ca4f609d
Kenji Matsumoto <sample@example.com>
2025-08-28 00:27:05 +0900

    login: Googleボタンの文言を『Googleアカウントでログイン』に変更。LINEボタンの上に配置済み

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -242,14 +242,13 @@ export function Login() {
         <div className="flex-grow border-t border-gray-200"></div>
       </div>
 
-      {/* Googleでログイン */}
+      {/* Googleアカウントでログイン */}
       <div className="mb-4">
         <Button
           onClick={() => void signInWithGoogle()}
           className="w-full bg-white text-gray-800 border border-gray-300 hover:bg-gray-50"
         >
-          <img src="/public/icons/icon_android_144x144.png" alt="" className="w-5 h-5 mr-2" />
-          Googleでログイン
+          Googleアカウントでログイン
         </Button>
       </div>
 
